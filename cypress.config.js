301047f8215ffb8cf14890e1889fd8a8e95cd88b
danish2705 <danishmeraj06@gmail.com>
2024-11-07 16:30:32 +0530

    fourth commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
-
+const { allureCypress } = require("allure-cypress/reporter");
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
+      allureCypress(on, config);
+      return config;
     },
   },
 });
